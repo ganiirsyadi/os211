9df62f1ad5ba53085e27642474628108487ca88c
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2021-03-17 17:22:42 +0700

    Add assignment Week 03

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Copyright (C) 2020-2021 Cicak Bin Kadal
 # https://www.youtube.com/watch?v=KAXK07ni9gU
 
@@ -15,7 +14,6 @@
 # ATTN:
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
-
 REC2="ganiilhamirsyadi@gmail.com"
 REC1="operatingsystems@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
@@ -24,39 +22,36 @@ SHA="SHA256SUM"
 [ -d $HOME/RESULT ] || mkdir -p $HOME/RESULT
 pushd $HOME/RESULT
 for II in W?? ; do
-	    [ -d $II ] || continue
-	        TARFILE=my$II.tar.bz2
-		    TARFASC=$TARFILE.asc
-		        rm -f $TARFILE $TARFASC
-			    echo "tar cfj $TARFILE $II/"
-			        tar cfj $TARFILE $II/
-				    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
-				        gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
-				done
-				popd
+	[ -d $II ] || continue
+	TARFILE=my$II.tar.bz2
+	TARFASC=$TARFILE.asc
+	rm -f $TARFILE $TARFASC
+	echo "tar cfj $TARFILE $II/"
+	tar cfj $TARFILE $II/
+	echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+	gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
 
-				rm -f $HOME/RESULT/fakeDODOL
-				for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
-					   echo "Check and move $II..."
-					      [ -f $II ] && mv -f $II .
-				      done
+rm -f $HOME/RESULT/fakeDODOL
+for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
+	echo "Check and move $II..."
+	[ -f $II ] && mv -f $II .
+done
 
-				      echo "rm -f $SHA $SHA.asc"
-				      rm -f $SHA $SHA.asc
+echo "rm -f $SHA $SHA.asc"
+rm -f $SHA $SHA.asc
 
-				      echo "sha256sum $FILES > $SHA"
-				      sha256sum $FILES > $SHA
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
 
-				      echo "sha256sum -c $SHA"
-				      sha256sum -c $SHA
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
 
-				      echo "gpg -o $SHA.asc -a -sb $SHA"
-				      gpg -o $SHA.asc -a -sb $SHA
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
 
-				      echo "gpg --verify $SHA.asc $SHA"
-				      gpg --verify $SHA.asc $SHA
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
 
-				      exit 0
-
-
-
+exit 0
